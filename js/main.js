360916e1418419dc86eb94254d2b12f3aeb8080b
JPerez1005 <perezbuenojuliandavid@gmail.com>
2023-09-05 19:16:06 -0500

    cambios

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//swiper slider
+
+var swiper = new Swiper(".bg_slider_thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+    });
+    var swiper2 = new Swiper(".bg_slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+//Navigation bar effects on scroll
+window.addEventListener('scroll', function(){
+    const header=this.document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>0);
+});
+
+//REsponsive navigation menu toggle
+
+const menuBtn=document.querySelector('.nav_menu_btn');
+const closeBtn=document.querySelector('.nav_close_btn');
+const navigation=document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', ()=>{
+    navigation.classList.add('active');
+});
+
+closeBtn.addEventListener('click',()=>{
+    navigation.classList.remove('active');
+});
